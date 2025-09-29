9a44bf0f48e1f480243ceef2629e8d8f5cb3c4b4
olgashu-tlu <olgashu@tlu.ee>
2025-09-29 19:51:46 +0300

    Lab7

---
+++ Labs/Lab7/script.js
@@ -0,0 +1,40 @@
+let firstName = "Olga";
+
+
+let newSentence = "Hello my name is " + firstName;
+let num1 = 6;
+const num2 = 8;
+
+let result = num1 + num2;
+
+console.log(result);
+console.log(newSentence);
+console.log(firstName);
+let mixedResult = firstName + num1;
+console.log(mixedResult);
+
+console.log(`Hello i am ${firstName}, i am called ${firstName}, call me ${firstName}`);
+
+console.log("Hello i am Olga, i am called OLga, call me Olga");
+let start = "Hello i am ";
+let myName = "Olga";
+let start2 = " ,i am called ";
+let start3 = " ,call me ";
+console.log(start + myName + start2 + myName + start3 +myName);
+
+
+let num5;
+const num6 = 10; 
+let result2 = num5 + num6;
+
+// alert("How are you?");
+
+
+let a = 13;
+let b = 3;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b );
