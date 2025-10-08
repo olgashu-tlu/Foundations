724468450c7879a3f26ba2d701da66a6bc14737c
olgashu-tlu <olgashu@tlu.ee>
2025-10-08 18:21:20 +0300

    Lab8

---
+++ Labs/Lab8/script.js
@@ -0,0 +1,32 @@
+let grades = [50,89,75,84,99];
+let sum = 0;
+for(let i = 0; i<5; i++)
+{
+    sum += grades[i];
+}
+
+console.log(sum);
+
+let result = sum/5;
+console.log(result);
+
+
+
+
+
+/*
+//Grade Calculator
+const grade = prompt("Enter the grade");
+if (grade >= 90)
+{
+    console.log("A");
+} else if(grade >=80)
+{
+    console.log("B");
+}else
+{
+    console.log("C");
+}
+
+*/
+
