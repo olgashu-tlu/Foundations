831f55c2197ca13af42de21dfdba557529dc6adc
olgashu-tlu <olgashu@tlu.ee>
2025-11-10 19:54:23 +0200

    Lab13
    Class work

---
+++ Labs/lab13/meal.js
@@ -0,0 +1,17 @@
+//1- Link to get a random meal
+let randomMealURL = 'https://www.themealdb.com/api/json/v1/1/random.php'
+
+//2- Link to lookup a specific meal with an id
+//https://www.themealdb.com/api/json/v1/1/lookup.php?i=
+
+//3- Link to search for meals using a keyword
+//https://www.themealdb.com/api/json/v1/1/search.php?s=
+
+
+const getRandomMeal = ()=> {
+fetch(randomMealURL)
+.then((item) =>item.json())
+.then((data)=>console.log(data.meals[0]))
+.catch ((err)=>console.log("The issue",err));
+}
+getRandomMeal();
